8257742f136e67f2de4f2e5efeb48712e8752f6a
Tobias Leupold <tobias.leupold@gmx.de>
2019-10-16 20:20:33 +0200

    Updated the copyright/license headers

--- b8/b8.php
+++ b8/b8.php
@@ -1,21 +1,22 @@
 <?php
 
-#   Copyright (C) 2006-2019 Tobias Leupold <tobias.leupold@web.de>
-#
-#   b8 - A statistical ("Bayesian") spam filter written in PHP 5
-#
-#   This program is free software; you can redistribute it and/or modify it
-#   under the terms of the GNU Lesser General Public License as published by
-#   the Free Software Foundation in version 2.1 of the License.
-#
-#   This program is distributed in the hope that it will be useful, but
-#   WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-#   or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
-#   License for more details.
-#
-#   You should have received a copy of the GNU Lesser General Public License
-#   along with this program; if not, write to the Free Software Foundation,
-#   Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+/* Copyright (C) 2006-2019 Tobias Leupold <tobias.leupold@gmx.de>
+
+   b8 - A statistical ("Bayesian") spam filter written in PHP
+
+   This program is free software; you can redistribute it and/or modify it
+   under the terms of the GNU Lesser General Public License as published by
+   the Free Software Foundation in version 2.1 of the License.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+   or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+   License for more details.
+
+   You should have received a copy of the GNU Lesser General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+*/
 
 /**
  * Copyright (C) 2006-2019 Tobias Leupold <tobias.leupold@web.de>
--- b8/storage/storage_mysql.php
+++ b8/storage/storage_mysql.php
@@ -1,21 +1,22 @@
 <?php
 
-#   Copyright (C) 2006-2014 Tobias Leupold <tobias.leupold@web.de>
-#
-#   This file is part of the b8 package
-#
-#   This program is free software; you can redistribute it and/or modify it
-#   under the terms of the GNU Lesser General Public License as published by
-#   the Free Software Foundation in version 2.1 of the License.
-#
-#   This program is distributed in the hope that it will be useful, but
-#   WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-#   or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
-#   License for more details.
-#
-#   You should have received a copy of the GNU Lesser General Public License
-#   along with this program; if not, write to the Free Software Foundation,
-#   Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+/* Copyright (C) 2006-2019 Tobias Leupold <tobias.leupold@gmx.de>
+
+   This file is part of the b8 package
+
+   This program is free software; you can redistribute it and/or modify it
+   under the terms of the GNU Lesser General Public License as published by
+   the Free Software Foundation in version 2.1 of the License.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+   or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+   License for more details.
+
+   You should have received a copy of the GNU Lesser General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+*/
 
 /**
  * The MySQL backend for communicating with the database, using the legacy mysql interface.
