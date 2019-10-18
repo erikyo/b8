f58054918f569c72402611c8e8f5ab770b1f8d4f
Tobias Leupold <tobias.leupold@gmx.de>
2019-10-18 16:38:28 +0200

    Updated the example for DBA use

--- example/index.php
+++ example/index.php
@@ -1,121 +1,69 @@
 <?php
 
-#   Copyright (C) 2006-2013 Tobias Leupold <tobias.leupold@web.de>
-#
-#   This file is part of the b8 package
-#
-#   This program is free software; you can redistribute it and/or modify it
-#   under the terms of the GNU Lesser General Public License as published by
-#   the Free Software Foundation in version 2.1 of the License.
-#
-#   This program is distributed in the hope that it will be useful, but
-#   WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-#   or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
-#   License for more details.
-#
-#   You should have received a copy of the GNU Lesser General Public License
-#   along with this program; if not, write to the Free Software Foundation,
-#   Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+/* Copyright (C) 2006-2019 Tobias Leupold <tobias.leupold@gmx.de>
 
+   b8 - A statistical ("Bayesian") spam filter written in PHP
 
-##### This is an example script demonstrating how b8 can be used. #####
+   This program is free software; you can redistribute it and/or modify it
+   under the terms of the GNU Lesser General Public License as published by
+   the Free Software Foundation in version 2.1 of the License.
 
-#/*
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+   or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+   License for more details.
 
-# Use this code block if you want to use Berkeley DB.
-
-# The database filename is interpreted relative to the b8.php script location.
-
-$config_b8 = array(
-	'storage' => 'dba'
-);
-
-$config_storage = array(
-	'database' => 'wordlist.db',
-	'handler'  => 'db4'
-);
-
-#*/
-
-/*
-
-# Use this code block if you want to use MySQL.
-
-# An existing link resource can be passed to b8 by setting
-# $config_database['connection'] to this link resource.
-
-$config_b8 = array(
-	'storage' => 'mysql'
-);
-
-$config_storage = array(
-	'database'   => 'test',
-	'table_name' => 'b8_wordlist',
-	'host'       => 'localhost',
-	'user'       => '',
-	'pass'       => ''
-);
-
+   You should have received a copy of the GNU Lesser General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 */
 
-/*
+/***************************************************************
+ * This is an example script demonstrating how b8 can be used. *
+ ***************************************************************/
 
-# Use this code block if you want to use PostgreSQL.
+// Use this code block if you want to use Berkeley DB (you should ;-).
 
-# An existing PDO instance can be passed to b8 by setting
-# $config_database['connection'] to this instance.
+// First create an empty database (e. g. using the install/setup_berkeleydb.php script)
+// and move it here, so that the following resource creation will work:
+$db = dba_open('wordlist.db', 'w', 'db4');
 
-$config_b8 = array(
-	'storage' => 'postgresql'
-);
+// Tell b8 to use the above Berkeley DB
+$config_b8      = [ 'storage'  => 'dba' ];
+$config_storage = [ 'resource' => $db   ];
 
-$config_storage = array(
-	'database'   => 'test',
-	'schema'     => 'b8',
-	'table_name' => 'b8_wordlist',
-	'host'       => 'localhost',
-	'port'       => '5432',
-	'user'       => '',
-	'pass'       => ''
-);
+// Tell b8 to use the new-style HTML extractor
+$config_lexer = [ 'old_get_html' => false,
+                  'get_html'     => true ];
 
-*/
+// Tell the degenerator to use multibyte operations
+// (needs PHP's mbstring module! If you don't have it, set 'multibyte' to false)
+$config_degenerator = [ 'multibyte' => true ];
 
-# Tell b8 to use the new-style HTML extractor
-$config_lexer = array(
-	'old_get_html' => FALSE,
-	'get_html'     => TRUE
-);
+/**************************
+ * Here starts the script *
+ **************************/
 
-# Tell the degenerator to use multibyte operations
-# (needs PHP's mbstring module! If you don't have it, set 'multibyte' to FALSE)
-$config_degenerator = array(
-	'multibyte' => TRUE
-);
-
-##### Here starts the script #####
-
-$time_start = NULL;
+$time_start = null;
 
 function microtimeFloat()
 {
-	list($usec, $sec) = explode(' ', microtime());
-	return((float) $usec + (float) $sec);
+    list($usec, $sec) = explode(' ', microtime());
+    return (float) $usec + (float) $sec;
 }
 
-# Output a nicely colored rating
+// Output a nicely colored rating
 
 function formatRating($rating)
 {
+    if ($rating === false) {
+        return '<span style="color:red">could not calculate spaminess</span>';
+    }
 
-	if($rating === FALSE)
-		return '<span style="color:red">could not calculate spaminess</span>';
-
-	$red   = floor(255 * $rating);
-	$green = floor(255 * (1 - $rating));
-
-	return "<span style=\"color:rgb($red, $green, 0);\"><b>" . sprintf("%5f", $rating) . "</b></span>";
-
+    $red   = floor(255 * $rating);
+    $green = floor(255 * (1 - $rating));
+    return "<span style=\"color:rgb($red, $green, 0);\"><b>" . sprintf("%5f", $rating)
+           . "</b></span>";
 }
 
 echo <<<END
@@ -148,83 +96,76 @@ END;
 
 $postedText = '';
 
-if(isset($_POST['action']) and $_POST['text'] ==  '')
-	echo "<p style=\"color:red;\"><b>Please type in a text!</b></p>\n\n";
+if (isset($_POST['action']) and $_POST['text'] ==  '') {
+    echo "<p style=\"color:red;\"><b>Please type in a text!</b></p>\n\n";
+} elseif(isset($_POST['action']) and $_POST['text'] != '') {
+    $time_start = microtimeFloat();
 
-elseif(isset($_POST['action']) and $_POST['text'] != '') {
+    // Include the b8 code
+    require __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'b8'
+            . DIRECTORY_SEPARATOR . 'b8.php';
 
-	$time_start = microtimeFloat();
+    # Create a new b8 instance
+    try {
+        $b8 = new b8\b8($config_b8, $config_storage, $config_lexer, $config_degenerator);
+    }
+    catch(Exception $e) {
+        echo "<b>example:</b> Could not initialize b8.<br/>\n";
+        echo "<b>Error message:</b> ", $e->getMessage();
+        echo "\n\n</div>\n\n</body>\n\n</html>";
+        exit();
+    }
 
-	# Include the b8 code
-	require dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'b8' . DIRECTORY_SEPARATOR . 'b8.php';
+    $text = stripslashes($_POST['text']);
+    $postedText = htmlentities($text, ENT_QUOTES, 'UTF-8');
 
-	# Create a new b8 instance
-	
-	try {
-		$b8 = new b8($config_b8, $config_storage, $config_lexer, $config_degenerator);
-	}
-	catch(Exception $e) {
-		echo "<b>example:</b> Could not initialize b8.<br />\n";
-		echo "<b>Error message:</b> ", $e->getMessage();
-		echo "\n\n</div>\n\n</body>\n\n</html>";
-		exit();
-	}
+    switch($_POST['action']) {
+        case 'Classify':
+            echo '<p><b>Spaminess: ' . formatRating($b8->classify($text)) . "</b></p>\n";
+            break;
 
-	$text = stripslashes($_POST['text']);
-	$postedText = htmlentities($text, ENT_QUOTES, 'UTF-8');
+        case 'Save as Spam':
+            $ratingBefore = $b8->classify($text);
+            $b8->learn($text, b8\b8::SPAM);
+            $ratingAfter = $b8->classify($text);
 
-	switch($_POST['action']) {
+            echo "<p>Saved the text as Spam</p>\n\n";
+            echo "<div><table>\n";
+            echo '<tr><td>Classification before learning:</td><td>' . formatRating($ratingBefore) . "</td></tr>\n";
+            echo '<tr><td>Classification after learning:</td><td>'  . formatRating($ratingAfter)  . "</td></tr>\n";
+            echo "</table></div>\n\n";
 
-		case 'Classify':
-			echo '<p><b>Spaminess: ' . formatRating($b8->classify($text)) . "</b></p>\n";
-			break;
+            break;
 
-		case 'Save as Spam':
+        case 'Save as Ham':
+            $ratingBefore = $b8->classify($text);
+            $b8->learn($text, b8\b8::HAM);
+            $ratingAfter = $b8->classify($text);
 
-			$ratingBefore = $b8->classify($text);
-			$b8->learn($text, b8::SPAM);
-			$ratingAfter = $b8->classify($text);
+            echo "<p>Saved the text as Ham</p>\n\n";
 
-			echo "<p>Saved the text as Spam</p>\n\n";
+            echo "<div><table>\n";
+            echo '<tr><td>Classification before learning:</td><td>' . formatRating($ratingBefore) . "</td></tr>\n";
+            echo '<tr><td>Classification after learning:</td><td>'  . formatRating($ratingAfter)  . "</td></tr>\n";
+            echo "</table></div>\n\n";
 
-			echo "<div><table>\n";
-			echo '<tr><td>Classification before learning:</td><td>' . formatRating($ratingBefore) . "</td></tr>\n";
-			echo '<tr><td>Classification after learning:</td><td>'  . formatRating($ratingAfter)  . "</td></tr>\n";
-			echo "</table></div>\n\n";
+            break;
 
-			break;
+        case 'Delete from Spam':
+            $b8->unlearn($text, b8\b8::SPAM);
+            echo "<p style=\"color:green\">Deleted the text from Spam</p>\n\n";
+            break;
 
-		case 'Save as Ham':
+        case 'Delete from Ham':
+            $b8->unlearn($text, b8\b8::HAM);
+            echo "<p style=\"color:green\">Deleted the text from Ham</p>\n\n";
+            break;
 
-			$ratingBefore = $b8->classify($text);
-			$b8->learn($text, b8::HAM);
-			$ratingAfter = $b8->classify($text);
+    }
 
-			echo "<p>Saved the text as Ham</p>\n\n";
-
-			echo "<div><table>\n";
-			echo '<tr><td>Classification before learning:</td><td>' . formatRating($ratingBefore) . "</td></tr>\n";
-			echo '<tr><td>Classification after learning:</td><td>'  . formatRating($ratingAfter)  . "</td></tr>\n";
-			echo "</table></div>\n\n";
-
-			break;
-
-		case 'Delete from Spam':
-			$b8->unlearn($text, b8::SPAM);
-			echo "<p style=\"color:green\">Deleted the text from Spam</p>\n\n";
-			break;
-
-		case 'Delete from Ham':
-			$b8->unlearn($text, b8::HAM);
-			echo "<p style=\"color:green\">Deleted the text from Ham</p>\n\n";
-			break;
-
-	}
-
-	$mem_used      = round(memory_get_usage() / 1048576, 5);
-	$peak_mem_used = round(memory_get_peak_usage() / 1048576, 5);
-	$time_taken    = round(microtimeFloat() - $time_start, 5);
-
+    $mem_used      = round(memory_get_usage() / 1048576, 5);
+    $peak_mem_used = round(memory_get_peak_usage() / 1048576, 5);
+    $time_taken    = round(microtimeFloat() - $time_start, 5);
 }
 
 echo <<<END
@@ -253,8 +194,7 @@ echo <<<END
 
 END;
 
-if($time_start !== NULL) {
-
+if($time_start !== null) {
 echo <<<END
 <div>
 <table border="0">
@@ -265,7 +205,6 @@ echo <<<END
 </div>
 
 END;
-
 }
 
 ?>
