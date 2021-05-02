d67715e871224a72fdf06eba13a1fd46eb418622
Tobias Leupold <tobias.leupold@gmx.de>
2021-05-02 12:37:23 +0200

    Added SPDX license header

--- b8/storage/sqlite.php
+++ b8/storage/sqlite.php
@@ -1,32 +1,17 @@
 <?php
 
-/* Copyright (C) 2019 Tobias Leupold <tobias.leupold@gmx.de>
+/* SPDX-FileCopyrightText: 2021 Malte Paskuda <malte@paskuda.biz>
 
-   This file is part of the b8 package
-
-   This program is free software; you can redistribute it and/or modify it
-   under the terms of the GNU Lesser General Public License as published by
-   the Free Software Foundation in version 2.1 of the License.
-
-   This program is distributed in the hope that it will be useful, but
-   WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-   or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
-   License for more details.
-
-   You should have received a copy of the GNU Lesser General Public License
-   along with this program; if not, write to the Free Software Foundation,
-   Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+   SPDX-License-Identifier: LGPL-2.1-only
 */
 
 namespace b8\storage;
 use PDO;
 
 /**
- * A sqlite storage backend
+ * An SQLite storage backend
  *
- * @license LGPL 2.1
  * @package b8
- * @author Malte Paskuda <malte@paskuda.biz>
  */
 
 class sqlite extends storage_base
@@ -53,7 +38,7 @@ class sqlite extends storage_base
         foreach ($tokens as $token) {
             $escaped[] = $this->sqlite->quote($token);
         }
-        
+
         $result = $this->sqlite->query('SELECT token, count_ham, count_spam'
                                       . ' FROM ' . $this->table
                                       . ' WHERE token IN '
@@ -74,7 +59,7 @@ class sqlite extends storage_base
         $query->bindParam(1, $token, PDO::PARAM_STR);
         $query->bindParam(2, $count[\b8\b8::KEY_COUNT_HAM], PDO::PARAM_INT);
         $query->bindParam(3, $count[\b8\b8::KEY_COUNT_SPAM], PDO::PARAM_INT);
-        
+
         $query->execute();
     }
 
