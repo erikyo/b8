a444a1cc4b55224aaa347773e5b77c6414e02ee0
Tobias Leupold <tl@stonemx.de>
2024-01-27 10:29:55 +0100

    Fixed a possible cross scripting vulnerability
    cf. https://patchstack.com/database/report-preview/dc9b64df-d927-46f2-bebe-532f08636c15?pin=fJLYPWbF2IEb08ar

--- install/setup_berkeleydb.php
+++ install/setup_berkeleydb.php
@@ -32,103 +32,82 @@ echo <<<END
 
 END;
 
+function setup_db()
+{
+    $dbfile = $_POST['dbfile'];
+    $dbfile_escaped = htmlentities($dbfile);
+    $dbfile_directory = $_SERVER['DOCUMENT_ROOT'] . dirname($_SERVER['PHP_SELF']);
+
+    echo 'Checking database file name &hellip; ';
+    if($dbfile == '') {
+        echo "<span style=\"color:red;\">Please provide the name of the database file!</span><br/>\n";
+        return false;
+    }
+    echo "$dbfile_escaped<br/>\n";
+
+    echo "Touching/Creating $dbfile_escaped &hellip; ";
+    if(touch($dbfile) === false) {
+        echo "<span style=\"color:red;\">Failed to touch the database file. Please check the given filename and/or fix the permissions of $dbfile_directory.</span><br/>\n";
+        return false;
+    }
+    echo "done<br/>\n";
+
+    echo 'Setting file permissions to 0666 &hellip; ';
+    if (chmod($dbfile, 0666) === false) {
+        echo "<span style=\"color:red;\">Failed to change the permissions of $dbfile_directory$dbfile_escaped. Please adjust them manually.</span><br />\n";
+        return false;
+    }
+    echo "done<br/>\n";
+
+    echo 'Checking if the given file is empty &hellip; ';
+    if (filesize($dbfile) > 0) {
+        echo "<span style=\"color:red;\">$dbfile_directory$dbfile_escaped is not empty. Can't create a new database. Please delete/empty this file or give another filename.</span><br />\n";
+        return false;
+    }
+    echo "it is<br/>\n";
+
+    echo "Connecting to $dbfile_escaped &hellip; ";
+    $db = dba_open($dbfile, "c", $_POST['handler']);
+    if ($db === false) {
+        echo "<span style=\"color:red;\">Could not connect to the database!</span><br/>\n";
+        return false;
+    }
+    echo "done<br/>\n";
+
+    echo 'Storing necessary internal variables &hellip; ';
+    $internals = [ 'b8*dbversion' => '3',
+                   'b8*texts'     => '0 0' ];
+    foreach($internals as $key => $value) {
+        if(dba_insert($key, $value, $db) === false) {
+            echo "<span style=\"color:red;\">Failed to insert data!</span><br/>\n";
+            return false;
+        }
+    }
+    echo "done<br/>\n";
+
+    echo 'Trying to read data from the database &hellip; ';
+    $dbversion = dba_fetch('b8*dbversion', $db);
+    if($dbversion != '3') {
+        echo "<span style=\"color:red;\">Failed to read data!</span><br />\n";
+        return false;
+    }
+    echo "success<br/>\n";
+
+    dba_close($db);
+    echo "</p>\n\n";
+    echo "<p style=\"color:green;\">Successfully created a new b8 database!</p>\n\n";
+    echo "<table>\n";
+    echo "<tr><td>Filename:</td><td>$dbfile_directory$dbfile_escaped</td></tr>\n";
+    echo "<tr><td>DBA handler:</td><td>{$_POST['handler']}</td><tr>\n";
+    echo "</table>\n\n";
+}
+
 $failed = false;
 
 if (isset($_POST['handler'])) {
-    $dbfile = $_POST['dbfile'];
-    $dbfile_directory = $_SERVER['DOCUMENT_ROOT'] . dirname($_SERVER['PHP_SELF']);
-
     echo "<h2>Creating database</h2>\n\n";
     echo "<p>\n";
-    echo 'Checking database file name &hellip; ';
-
-    if($dbfile == '') {
-        echo "<span style=\"color:red;\">Please provide the name of the database file!</span><br/>\n";
-        $failed = true;
-    } else {
-        echo "$dbfile<br/>\n";
-    }
-
-    if (! $failed) {
-        echo "Touching/Creating " . htmlentities($dbfile) . " &hellip; ";
-        if(touch($dbfile) === false) {
-            echo "<span style=\"color:red;\">Failed to touch the database file. Please check the given filename and/or fix the permissions of $dbfile_directory.</span><br/>\n";
-            $failed = true;
-        } else {
-            echo "done<br/>\n";
-        }
-    }
-
-    if (! $failed) {
-        echo 'Setting file permissions to 0666 &hellip; ';
-        if (chmod($dbfile, 0666) === false) {
-            echo "<span style=\"color:red;\">Failed to change the permissions of $dbfile_directory/$dbfile. Please adjust them manually.</span><br />\n";
-            $failed = true;
-        } else {
-            echo "done<br />\n";
-        }
-    }
-
-    if (! $failed) {
-        echo 'Checking if the given file is empty &hellip; ';
-        if (filesize($dbfile) > 0) {
-            echo "<span style=\"color:red;\">$dbfile_directory/$dbfile is not empty. Can't create a new database. Please delete/empty this file or give another filename.</span><br />\n";
-            $failed = true;
-        } else {
-            echo "it is<br/>\n";
-        }
-    }
-
-    if (! $failed) {
-        echo "Connecting to $dbfile &hellip; ";
-        $db = dba_open($dbfile, "c", $_POST['handler']);
-        if ($db === false) {
-            echo "<span style=\"color:red;\">Could not connect to the database!</span><br/>\n";
-            $failed = true;
-        } else {
-            echo "done<br />\n";
-        }
-    }
-
-    if (! $failed) {
-        echo 'Storing necessary internal variables &hellip; ';
-        $internals = [ 'b8*dbversion' => '3',
-                       'b8*texts'     => '0 0' ];
-
-        foreach($internals as $key => $value) {
-            if(dba_insert($key, $value, $db) === false) {
-                echo "<span style=\"color:red;\">Failed to insert data!</span><br/>\n";
-                $failed = true;
-                break;
-            }
-        }
-
-        if (! $failed) {
-            echo "done<br/>\n";
-        }
-    }
-
-    if (! $failed) {
-        echo 'Trying to read data from the database &hellip; ';
-        $dbversion = dba_fetch('b8*dbversion', $db);
-        if($dbversion != '3') {
-            echo "<span style=\"color:red;\">Failed to read data!</span><br />\n";
-            $failed = true;
-        } else {
-            echo "success<br/>\n";
-        }
-    }
-
-    if (! $failed) {
-        dba_close($db);
-        echo "</p>\n\n";
-        echo "<p style=\"color:green;\">Successfully created a new b8 database!</p>\n\n";
-        echo "<table>\n";
-        echo "<tr><td>Filename:</td><td>$dbfile_directory$dbfile</td></tr>\n";
-        echo "<tr><td>DBA handler:</td><td>{$_POST['handler']}</td><tr>\n";
-        echo "</table>\n\n";
-    }
-
+    $failed = ! setup_db();
     echo "</p>\n\n";
 }
 
